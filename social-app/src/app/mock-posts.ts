54b112e054682dc5a1d2607529d5cc137415f69e
Mark Smith <smith.h.mark@gmail.com>
2016-12-10 17:46:46 -0800

    created mock-data file
    following tutorial's example, I've created a file to hold the mock data
    and also created a Post class

---
+++ social-app/src/app/mock-posts.ts
@@ -0,0 +1,7 @@
+import { Post } from './post'
+
+export const POSTS: Post[] = [
+  { username:'@dickeyxxx', body: 'Node Rules' },
+  { username:'@jeffdickey', body:'trying out angular2' },
+  { username:'@smithhmark', body:'this post is in the mock-data' },
+];
---
+++ social-app/src/app/post.ts
@@ -0,0 +1,4 @@
+export class Post {
+  username: string;
+  body: string;
+}
