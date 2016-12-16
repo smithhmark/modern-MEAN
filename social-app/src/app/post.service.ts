aad2594394b52b835044fe368386aca3816e1e91
Mark Smith <smith.h.mark@gmail.com>
2016-12-15 16:49:40 -0800

    post service now gets from api
    PostService.getPosts now uses Http to get all posts from the api.

--- social-app/src/app/post.service.ts
+++ social-app/src/app/post.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Post } from './post';
 import { POSTS } from './mock-posts';
 
 @Injectable()
 export class PostService {
+  private postsUrl = 'api/posts';
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getPosts(): Promise<Post[]> { 
-    return this.getPostsInstantly();
-    //return this.getPostsSlowly();
+    return this.getPostsHTTP();
+  }
+
+  getPostsHTTP(): Promise<Post[]> {
+    return this.http.get(this.postsUrl)
+                .toPromise()
+                .then(response => response.json() as Post[])
+                .catch(this.handleError);
   }
 
   getPostsInstantly(): Promise<Post[]> {
@@ -21,5 +31,10 @@ export class PostService {
     return new Promise<Post[]>(resolve =>
                                setTimeout(resolve, 2000)) // delay 2 seconds
                                .then(() => this.getPostsInstantly());
+  }
+
+  handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // debugging only
+    return Promise.reject(error.message || error);
   }
 }
